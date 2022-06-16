6ca9888ad6de27b7e01b3d3ebe255a130d325b1f
wilfried adjei <adjeiwilfried@gmail.com>
2022-06-17 01:58:46 +0200

    premiere version de mon site ong

---
+++ img/entreprise.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 31 mai 2022 à 14:28
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `entreprise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+DROP TABLE IF EXISTS `employes`;
+CREATE TABLE IF NOT EXISTS `employes` (
+  `id_employes` int(3) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) DEFAULT NULL,
+  `prenom` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `date_embauche` date DEFAULT NULL,
+  PRIMARY KEY (`id_employes`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `employes`
+--
+
+INSERT INTO `employes` (`id_employes`, `nom`, `prenom`, `email`, `date_embauche`) VALUES
+(1, NULL, NULL, NULL, NULL),
+(2, NULL, NULL, NULL, NULL),
+(3, NULL, NULL, NULL, NULL),
+(4, NULL, NULL, NULL, NULL),
+(5, NULL, NULL, NULL, NULL),
+(6, NULL, NULL, NULL, NULL),
+(7, NULL, NULL, NULL, NULL),
+(8, NULL, NULL, NULL, NULL),
+(9, NULL, NULL, NULL, NULL),
+(10, NULL, NULL, NULL, NULL),
+(11, NULL, NULL, NULL, NULL),
+(12, NULL, NULL, NULL, NULL),
+(13, NULL, NULL, NULL, NULL),
+(14, NULL, NULL, NULL, NULL),
+(15, NULL, NULL, NULL, NULL),
+(16, NULL, NULL, NULL, NULL),
+(17, NULL, NULL, NULL, NULL),
+(18, NULL, NULL, NULL, NULL),
+(19, NULL, NULL, NULL, NULL),
+(20, NULL, NULL, NULL, NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
